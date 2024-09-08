f6efca5bd757c2a3885b48579da0f5337f0cf867
Donata <donatageen@live.co.za>
2024-09-08 11:43:00 +0200

    Added some UI assets for buying and placing towers.
    (cherry picked from commit 0f5dce01c69b534eb5beca4d6572f88bf77bd02d)

---
+++ Assets/Scripts/PlatformInteractions.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformInteractions : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        //do the things.
+    }
+}
